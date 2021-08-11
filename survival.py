caf014cb2baaac0a7cf15f977f6473fd7a10464a
Peter Chen <chenjiajun0825@gmail.com>
2021-08-11 09:51:40 -0400

    Improved terrain generation to have multiple blocks in rows

--- survival.py
+++ survival.py
@@ -68,14 +68,14 @@ class Mario(object):
     # Moves mario to the right by xVelocity amount
     def moveRight(self, app):
 
-        collided = app.map.collided(self.top, self.left + Mario.width,
-                            self.top + Mario.height, self.left,
-                            self.xVelocity, self.yVelocity)
+        # collided = app.map.collided(self.top, self.left + Mario.width,
+        #                     self.top + Mario.height, self.left,
+        #                     self.xVelocity, self.yVelocity)
 
-        if collided[0] and collided[1] == "left":
-            self.xVelocity = 0
-            self.left = collided[2] - Mario.width
-            return
+        # if collided[0] and collided[1] == "left":
+        #     self.xVelocity = 0
+        #     self.left = collided[2] - Mario.width
+        #     return
 
         # Sets the initial sprite to right if it weren't
         if(self.spriteX < len(Mario.sprites) // 2 + 1):
@@ -100,14 +100,14 @@ class Mario(object):
     # Moves mario to the left by -xVelocity amout
     def moveLeft(self, app):
 
-        collided = app.map.collided(self.top, self.left + Mario.width,
-                            self.top + Mario.height, self.left,
-                            self.xVelocity, self.yVelocity)
+        # collided = app.map.collided(self.top, self.left + Mario.width,
+        #                     self.top + Mario.height, self.left,
+        #                     self.xVelocity, self.yVelocity)
 
-        if collided[0] and collided[1] == "right":
-            self.xVelocity = 0
-            self.left = collided[2]
-            return
+        # if collided[0] and collided[1] == "right":
+        #     self.xVelocity = 0
+        #     self.left = collided[2]
+        #     return
 
         # Sets initial sprite to left if it weren't
         if(self.spriteX >= len(Mario.sprites) // 2 + 1):
@@ -193,15 +193,16 @@ class Mario(object):
 
     def fall(self, map):
 
-        pass
+        if self.yMotion == 1: return
 
-        # collided = map.collided(self.top, self.left + Mario.width,
-        #                     self.top + Mario.height, self.left,
-        #                     0, -2)
+        collided = map.collided(self.top, self.left + Mario.width,
+                            self.top + Mario.height, self.left,
+                            0, 5)
 
-        # if not collided[0]:
-        #     self.yMotion = 0
-        #     self.yVelocity = 0.5
+        if not collided[0] and self.top < map.height - 89:
+            if self.yVelocity == 0:
+                self.yMotion = 0
+                self.yVelocity = 5
 
     # Does one small part of jumping at a time
     def jump(self, map):
@@ -209,7 +210,7 @@ class Mario(object):
         # Sets initial yVelocity to 35 if mario were static
         if self.yVelocity == 0:
 
-            self.yVelocity = 35
+            self.yVelocity = 20
 
         # If character is falling below the leveled ground then set
         # character to be on the ground
@@ -220,18 +221,19 @@ class Mario(object):
             self.yVelocity = 0
 
         # If character is falling, increase top margin by yVelocity
-        # Then increase falling speed by 1.4 times
+        # Then increase falling speed by 1.25 times
         elif self.yMotion == 0:
 
             self.top += self.yVelocity
-            self.yVelocity *= 1.4
+            if self.yVelocity < 30:
+                self.yVelocity *= 1.25
         
         # If character is jumping, decrease top margin by yVelocity
         # Then decrease speed going up by 1.3 times
         else:
 
             self.top -= self.yVelocity
-            self.yVelocity /= 1.3
+            self.yVelocity /= 1.2
         
         # If yVelocity is near 0, set it to 0
         if self.yVelocity < 3:
@@ -395,36 +397,46 @@ class Blocks(object):
 
         self.blocks = []
 
-        self.startPos = Block(app, blockWidth, 1, blockWidth * 5)
+        for i in range(6, 10):
+            self.blocks.append(Block(app, blockWidth, 1, blockWidth * i, 5, i - 4))
 
-        self.finalBlock = Block(app, blockWidth, 2, mapWidth - blockWidth * 5)
+        self.startPos = Block(app, blockWidth, 1, blockWidth * 5, 5, 0)
 
         self.blocks.append(self.startPos)
+
+        for i in range(0, 5):
+            self.blocks.append(Block(app, blockWidth, 2, mapWidth - blockWidth * i, 5, 4 - i))
+
+        self.finalBlock = Block(app, blockWidth, 2, mapWidth - blockWidth * 5, 5, 0)
         
         self.blocks.append(self.finalBlock)
 
         while not self.completeLevel():
             pass
+
+        for block in self.blocks:
+            if block == self.startPos or block == self.finalBlock: continue
+            block.addGoomba()
     
 
-    def getNewBlock(self, blocksTried):
+    def getNewBlock(self, blocksTried, length):
 
-        level = random.randint(1, 3)
+        level = random.randint(1, 4)
         leftPosition = random.randint(0, self.maxBlocks)
 
-        thisBlock = Block(self.app, self.blockWidth, level, self.blockWidth * leftPosition)
+        thisBlock = Block(self.app, self.blockWidth, level, self.blockWidth * leftPosition, length, 0)
 
         for block in self.blocks:
 
             if block.left == thisBlock.left:
 
-                return self.getNewBlock(blocksTried)
+                return self.getNewBlock(blocksTried, length)
         
         for block in blocksTried:
 
             if block == thisBlock:
 
-                return self.getNewBlock(blocksTried)
+                return self.getNewBlock(blocksTried, length)
         
         return thisBlock
     
@@ -464,15 +476,34 @@ class Blocks(object):
         
         return False
 
+    def numOfBlocks(self):
+
+        rand = random.random()
+
+        if rand < 0.2:
+            return 1
+        elif rand < 0.5:
+            return 2
+        elif rand < 0.8:
+            return 3
+        else:
+            return 4
+
     def completeLevel(self):
 
         blocksTried = []
 
         while len(blocksTried) < 10:
 
-            newBlock = self.getNewBlock(blocksTried)
+            numOfBlocks = self.numOfBlocks()
+            newBlock = self.getNewBlock(blocksTried, numOfBlocks)
 
-            self.blocks.append(newBlock)
+            newBlocks = [newBlock]
+
+            for i in range(numOfBlocks):
+                newBlocks.append(Block(self.app, self.blockWidth, newBlock.level, newBlock.left + self.blockWidth * (i + 1), numOfBlocks, i + 1))
+
+            self.blocks = self.blocks + newBlocks
 
             if self.blockIsReachebleHelper(self.startPos, newBlock):
 
@@ -487,7 +518,9 @@ class Blocks(object):
                         return True
         
             blocksTried.append(newBlock)
-            self.blocks.remove(newBlock)
+
+            for block in newBlocks:
+                self.blocks.remove(block)
         
         return False
 
@@ -518,7 +551,7 @@ class Block(object):
     block = None
     blockWidth = 0
 
-    def __init__(self, app, blockWidth, level, left):
+    def __init__(self, app, blockWidth, level, left, length, index):
 
         if Block.block == None:
             Block.block = (app.loadImage('./assets/images/tiles.png')
@@ -528,23 +561,36 @@ class Block(object):
             Block.blockWidth = blockWidth
         
         self.level = level
-        self.top = app.height - blockWidth * 2 - blockWidth * 4 * level
+        self.top = app.height - blockWidth * 2 - blockWidth * 3 * level
         self.left = left
+
+        self.index = index
+        self.length = length
+
+        self.goomba = None
+
+        self.app = app
+    
+    def addGoomba(self):
+
+        if self.length >= 3 and self.index == 0 and random.random() <= 0.7:
+            self.goomba = Goomba(self.top - Goomba.height, self.left)
+            self.app.goombas.append(self.goomba)
     
     def collided(self, top, right, bottom, left, xVelocity, yVelocity):
 
         if xVelocity != 0 and (top > self.top - Block.blockWidth and bottom < self.top):
 
-            if right < self.left and right + xVelocity > self.left:
+            if right < self.left + Block.blockWidth and right - xVelocity > self.left:
                 return (True, "right", self.left)
-            elif left > self.left + Block.blockWidth and left + xVelocity < self.left + Block.blockWidth:
+            elif left > self.left and left - xVelocity < self.left + Block.blockWidth:
                 return (True, "left", self.left + Block.blockWidth)
         
         if yVelocity != 0 and (right > self.left and left < self.left + Block.blockWidth):
 
-            if top > self.top + Block.blockWidth and top - yVelocity < self.top + Block.blockWidth:
+            if top > self.top and top - yVelocity < self.top + Block.blockWidth:
                 return (True, "top", self.top + Block.blockWidth)
-            elif bottom < self.top and bottom + yVelocity > self.top:
+            elif bottom < self.top + Block.blockWidth and bottom + yVelocity > self.top:
                 return (True, "bottom", self.top)
         
         return (False, 0, 0)
@@ -560,11 +606,50 @@ class Block(object):
     
     def drawBlock(self, canvas):
 
+        if self.left > self.app.width + self.app.map.leftShift: return
+
         cx = self.left + Block.blockWidth / 2
         cy = self.top + Block.blockWidth / 2
 
         canvas.create_image(cx, cy, 
                                 image=ImageTk.PhotoImage(Block.block))
+        canvas.create_text(cx, cy, text=f"{self.index}", fill="white")
+
+
+
+class Goomba(object):
+
+    sprites = []
+    width = 32
+    height = 32
+
+    def initialize(app):
+
+        sprite1 = (app.loadImage('./assets/images/enemies.png')
+                                        .crop((0, 4, 16, 20))
+                                        .resize((Goomba.width, Goomba.height)))
+        sprite2 = (app.loadImage('./assets/images/enemies.png')
+                                        .crop((20, 4, 36, 20))
+                                        .resize((Goomba.width, Goomba.height)))
+        Goomba.sprites.append(sprite1)
+        Goomba.sprites.append(sprite2)
+    
+    def __init__(self, top, left):
+        self.top = top
+        self.left = left
+        self.sprite = 0
+
+    def scrollGoomba(self, xVelocity):
+        self.left -= xVelocity
+    
+    def drawGoomba(self, canvas):
+
+        cx = self.left + Goomba.width / 2
+        cy = self.top + Goomba.height / 2
+
+        canvas.create_image(cx, cy, 
+                                image=ImageTk.PhotoImage(Goomba.sprites[self.sprite]))
+
 
 ###################
 #  Survival Map
@@ -579,10 +664,11 @@ class Survival(object):
 
         self.leftShift = 0
 
-        self.maxWidth = 1920
+        self.maxWidth = 3584
+        self.height = app.height
         
         # Sets left and right margin for when map scrolls
-        self.margin = 150
+        self.margin = 250
 
         # Sets width and height of blocks to be 32
         self.blockDimension = 32
@@ -625,6 +711,9 @@ class Survival(object):
 
         # Moves the clouds
         self.clouds.moveClouds(xVelocity)
+
+        for goomba in self.app.goombas:
+            goomba.scrollGoomba(xVelocity)
     
 
     # Draw background and everything in map
@@ -639,6 +728,8 @@ class Survival(object):
 def survival_redrawAll(app, canvas):
     app.map.drawMap(canvas)
     app.mario.drawMario(canvas)
+    for goomba in app.goombas:
+        goomba.drawGoomba(canvas)
 
 # Controller
 def survival_timerFired(app):
@@ -693,6 +784,8 @@ def appStarted(app):
     app.mode = "titleScreen"
     app.timerDelay = 20
 
+    app.goombas = []
+
 # Draws title screen
 def titleScreen_redrawAll(app, canvas):
 
@@ -713,7 +806,10 @@ def titleScreen_keyPressed(app, event):
         app.mode = "survival"
         app.map = Survival(app)
         Mario.initialize(app)
-        # app.mario = Mario(80, 385)
-        app.mario = Mario(160, app.height - 217)
+        app.mario = Mario(160, app.height - 188)
+        app.mario.jump(app.map)
+
+        Goomba.initialize(app)
+        # app.goombas.append(Goomba(200, 400))
 
 runApp(width=1280, height=480)
